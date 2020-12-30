1ad59af93964c0c96ba290fa62f2e65740b31c60
bratishques <gurda999@gmail.com>
2020-12-30 21:23:44 +0300

    Styling Travis-CI build fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
+
 import "tailwindcss/tailwind.css";
+import '../styles/globals.css'
 import Layout from "../components/layout"
 import {ScreenSizeProvider} from "../context/screenSizeContext"
 import {SidebarContextProvider} from "../context/sidebarContext"
