fc5a19651443f1920ad8da2c61586e8f27b3f0aa
bratishques <gurda999@gmail.com>
2020-12-30 21:19:45 +0300

    Styles fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/header'
-import styles from '../styles/Home.module.css'
+import '../styles/globals.css'
 
 export default function Home() {
   return (
