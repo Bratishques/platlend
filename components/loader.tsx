e101839ba75267425bcd097cf69e027fa272a66f
bratishques <gurda999@gmail.com>
2021-01-15 19:25:58 +0300

    Loader fix

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,5 +1,5 @@
 const Loader = ({ children }) => {
-  return <div className={`fixes w-full h-screen z-50 flex items-center justify-center bg-primary-bg `}>{children}</div>;
+  return <div className={`fixed w-full h-screen z-50 flex items-center justify-center bg-primary-bg `}>{children}</div>;
 };
 
 export default Loader;
